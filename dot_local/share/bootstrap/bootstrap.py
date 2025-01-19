7949ce26bddcd2d05b14c034a68dbdad8974273a
pidgey <pieter-jan@pidgey.net>
2025-01-19 13:05:59 +0100

    add screen bootstrap

--- dot_local/share/bootstrap/bootstrap.py
+++ dot_local/share/bootstrap/bootstrap.py
@@ -3,14 +3,45 @@ import argparse
 import os
 import json
 import subprocess
+import sys
 
 _HOME_     = os.getenv("HOME")
 _CONF_DIR_ = os.path.join(_HOME_, ".local", "share", "bootstrap")
 _CONF_     = os.path.join(_CONF_DIR_, "config.json")
 
+# Helper functions
+
 def read_settings():
-    with open(_CONF_, 'r') as f:
-        return json.loads(f.read())
+    try:
+        with open(_CONF_, 'r') as f:
+            return json.loads(f.read())
+    except Exception as e:
+        sys.exit(f"Could not read the config file!\n{e}")
+
+def read_file(path):
+    try:
+        with open(path, 'r') as f:
+            return f.read()
+    except:
+        return ""
+
+def get_connected_displays():
+    dir = '/sys/class/drm'
+    connected = [dev for dev in os.listdir(dir)
+                   if read_file( os.path.join(dir, dev, 'status') ) == "connected\n"]
+    return [dev.split("-", 1)[1] for dev in connected if dev.startswith("card") and "-" in dev]
+
+def get_active_screen_config():
+    settings  = read_settings()
+    connected = set(get_connected_displays())
+    for config in settings.get('output',{}).get('screen_configs', []):
+        if set([x['name'] for x in config]) == connected:
+            return config
+    return None
+
+
+
+# Bootstrap functions
 
 def bootstrap(desktop):
     print("Running bootstrap...")
@@ -26,19 +57,38 @@ def keyboard(desktop):
     if desktop == "sway":
         subprocess.run(["swaymsg", "input", "type:keyboard", "xkb_layout", f'"{layout}"'], check=True)
 
+def screens(desktop):
+    if not desktop:
+        print("Desktop Environment not specified"); return
+    desktop = desktop.lower()
+    config = get_active_screen_config()
+    if not config:
+        print("No config specified")
+        return
+    for screen in config:
+        n, x, y, s = screen['name'], screen['pos_x'], screen['pos_y'], screen['scale']
+        if desktop == "sway":
+            subprocess.run(f"swaymsg 'output {n} pos {x} {y} scale {s}'", shell=True)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="A script to handle various setups.")
     subparsers = parser.add_subparsers(dest="command", required=True)
 
     # Bootstrap command
-    parser_bootstrap = subparsers.add_parser("bootstrap", help="Run the bootstrap setup")
-    parser_bootstrap.add_argument("extra_arg", nargs="?", default=None, help="Optional extra argument for bootstrap")
-    parser_bootstrap.set_defaults(func=bootstrap)
+    p_bootstrap = subparsers.add_parser("bootstrap", help="Run the bootstrap setup")
+    p_bootstrap.add_argument("desktop", nargs="?", default=None, help="Specify the desktop used")
+    p_bootstrap.set_defaults(func=bootstrap)
 
     # Keyboard command
-    parser_keyboard = subparsers.add_parser("keyboard", help="Configure the keyboard")
-    parser_keyboard.add_argument("extra_arg", nargs="?", default=None, help="Optional extra argument for keyboard")
-    parser_keyboard.set_defaults(func=keyboard)
+    p_keyboard = subparsers.add_parser("keyboard", help="Set the keyboard layout")
+    p_keyboard.add_argument("desktop", nargs="?", default=None, help="Specify the desktop used")
+    p_keyboard.set_defaults(func=keyboard)
+
+    # Screens command
+    p_screens = subparsers.add_parser("screens", help="Set the screen layout")
+    p_screens.add_argument("desktop", nargs="?", default=None, help="Specify the desktop used")
+    p_screens.set_defaults(func=screens)
 
     args = parser.parse_args()
-    args.func(args.extra_arg)
+    args.func(args.desktop)
--- run_bootstrap.py
+++ run_bootstrap.py
@@ -7,6 +7,7 @@
 #
 
 # Imports
+import copy
 import os
 import json
 import subprocess
@@ -18,17 +19,25 @@ _HOME_     = os.getenv("HOME")
 _CONF_DIR_ = os.path.join(_HOME_, ".local", "share", "bootstrap")
 _CONF_     = os.path.join(_CONF_DIR_, "config.json")
 
+_PLAYBOOKS = [("Base system",  'base'),
+              ("Workstation",  'workstation'),
+              ("Server",       'server'),
+              ("Development",  'development'),
+              ("Work Desktop", 'work')]
+
 _SETTINGS  = {
-                  'bootstrap': {
-                      'base': False,
-                      'workstation': False,
-                      'server': False,
-                      'development': False,
-                      'work': False},
+                  'bootstrap': { i[1]: None for i in _PLAYBOOKS },
                   'input': {
-                      'keyboard': ''}
+                      'keyboard': None},
+                  'output': {
+                      'screen_configs': []
+                  }
              }
 
+_SCREEN = {key: None for key in ['name', 'pos_x', 'pos_y', 'scale']}
+
+#####
+# Helper Functions
 
 def confirm(text):
     fd = sys.stdin.fileno()
@@ -45,29 +54,73 @@ def confirm(text):
     return key.lower() == "y"
 
 
+def deep_update(original, update):
+    for key, value in update.items():
+        if isinstance(value, dict) and key in original and isinstance(original[key], dict):
+            deep_update(original[key], value)
+        else:
+            original[key] = value
+
+def read_file(path):
+    try:
+        with open(path, 'r') as f:
+            return f.read()
+    except:
+        return ""
+
+#####
+# Bootstrap functions
+
 def do_bootstrap(text, setting):
     r = confirm(f"[BOOTSTRAP] {text} bootstrap?")
-    _SETTINGS['bootstrap'][setting] = r
+
+def get_connected_displays():
+        dir = '/sys/class/drm'
+        connected = [dev for dev in os.listdir(dir)
+                         if read_file( os.path.join(dir, dev, 'status') ) == "connected\n"]
+        return [dev.split("-", 1)[1] for dev in connected if dev.startswith("card") and "-" in dev]
 
 
-def interact_settings():
-    if os.path.exists(_CONF_):
-        return
-    print("Creating config file:")
-    do_bootstrap("Base system",  'base')
-    do_bootstrap("Workstation",  'workstation')
-    do_bootstrap("Server",       'server')
-    do_bootstrap("Development",  'development')
-    do_bootstrap("Work Desktop", 'work')
-    code = input("[SETTINGS] Preferred keyboard layout code: ")
-    _SETTINGS['input']['keyboard'] = code
+def interact_settings(settings):
+    # Set playbooks
+    for desc, playbook in _PLAYBOOKS:
+        if not settings['bootstrap'][playbook] == None:  # The variable has been set
+            continue
+        settings['bootstrap'][playbook] = confirm(f"[BOOTSTRAP] {desc} bootstrap?")
+    # Set keyboard
+    if not settings['input']['keyboard']:
+        code = input("[SETTINGS] Preferred keyboard layout code: ")
+        settings['input']['keyboard'] = code
+    # Set displays
+    connected = set(get_connected_displays())
+    current_config_known = False
+    for config in settings['output']['screen_configs']:
+        if set([x['name'] for x in config]) == connected:
+            current_config_known = True
+            break
+    if current_config_known == False:
+        if confirm("[SETTINGS] Current screen-set-up has no config. Make one now?"):
+            conf = []
+            for screen in connected:
+                print(f" == Screen: {screen} ==")
+                s = copy.deepcopy(_SCREEN)
+                s['name'] = screen
+                for key in [k for k in _SCREEN.keys() if k != "name"]:
+                    s[key] = input(f"{key}: ")
+                conf.append(s)
+            settings['output']['screen_configs'].append(conf)
+    # Save config
     with open(_CONF_, 'w') as f:
-        f.write(json.dumps(_SETTINGS))
+        f.write(json.dumps(settings, indent=2))
 
 
 def read_settings():
+    settings = copy.deepcopy(_SETTINGS)
+    if not os.path.exists(_CONF_):
+        print("No settings file found... Generating a new one.")
     with open(_CONF_, 'r') as f:
-        return json.loads(f.read())
+        deep_update(settings, json.loads(f.read()))
+        return settings
 
 
 def bootstrap(settings):
@@ -83,6 +136,6 @@ def bootstrap(settings):
         subprocess.run(['ansible-playbook', file, "--ask-become-pass"], check=True)
 
 if __name__ == "__main__":
-    interact_settings()
     settings = read_settings()
+    interact_settings(settings)
     bootstrap(settings)
